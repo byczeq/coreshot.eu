3e3e3ac33833e419e9d701684a48309614c38acd
Marcin Mazurek <byq_pl@op.pl>
2025-11-05 08:58:43 +0100

    Updated index.ts

--- supabase/functions/subscribe/index.ts
+++ supabase/functions/subscribe/index.ts
@@ -17,7 +17,7 @@ Deno.serve(async (req: Request) => {
 
   try {
     const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
-    const supabaseKey = Deno.env.get("SUPABASE_ANON_KEY")!;
+    const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
     const supabase = createClient(supabaseUrl, supabaseKey);
 
     const { email, locale } = await req.json();
