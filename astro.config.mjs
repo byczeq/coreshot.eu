acbee09314ebf4a910d0b7e04cb7be772b108785
Marcin Mazurek <byq_pl@op.pl>
2025-11-05 11:15:50 +0100

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: cloudflare({
-    mode: 'directory'
-  }),
-
   vite: {
     plugins: [tailwindcss()]
   },
